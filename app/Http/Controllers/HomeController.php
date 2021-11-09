b3283e581889d035cc8af22e973c5ee9c601e75f
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-09 07:55:17 -0500

    Middleware admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        // $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
@@ -24,5 +24,15 @@ class HomeController extends Controller
     public function index()
     {
         return view('welcome');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function adminHome()
+    {
+        return 'HomeAdmin';
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('admin.home');
 });
+Route::get('/home', function() {
+    return "ADMIN";
+})->name('admin.home')->middleware('is_admin');
