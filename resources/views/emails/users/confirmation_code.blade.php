546208efd1f48fb2422fb26e839c67478efe2906
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-10 23:53:57 -0500

    Mejora en el correo de confirmación de usuario

--- resources/views/emails/users/confirmation_code.blade.php
+++ resources/views/emails/users/confirmation_code.blade.php
@@ -1,15 +1,38 @@
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
-    <h2>Hola {{ $name }}, gracias por registrarte en <strong>BIG Promo</strong> !</h2>
-    <p>Por favor confirma tu correo electrónico.</p>
-    <p>Para ello simplemente debes hacer click en el siguiente enlace:</p>
 
-    <a href="{{ url('/register/verify/' . $confirmation_code) }}">
-        Clic para confirmar tu email
-    </a>
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="app">
+            <div class='logo'>
+                <img src="{{ asset('img/logo.png') }}" alt="">
+            </div>
+            <h2>Hola {{ $name }} {{ $lastname }}, gracias por registrarte en <strong>BIG Promo</strong> !</h2>
+            <p>Por favor confirma tu correo electrónico.</p>
+            <p>Para ello simplemente debes hacer click en el siguiente enlace:</p>
+
+            <a href="{{ url('/register/verify/' . $confirmation_code) }}">
+                Click para confirmar tu email
+            </a>
+        </div>
+    </body>
 </html>
