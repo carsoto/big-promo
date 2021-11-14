b0d5576d44f305a11024a28227e9197e7f7b56f2
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-14 09:15:20 -0500

    Linkeadas la redes sociales con los iconos, eliminados el icono de tiktok, twiter y youtube

--- resources/views/common/user-footer.blade.php
+++ resources/views/common/user-footer.blade.php
@@ -9,11 +9,11 @@
         </div>
         @endif
         <div class="socials">
-            <a href="#"><i class="fab fab-icon-social fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fab-icon-social fa-instagram"></i></a>
-            <a href="#"><i class="fab fab-icon-social fa-twitter"></i></a>
+            <a href="https://www.facebook.com/RefrescoBigCola/?brand_redir=128701980519740"><i class="fab fab-icon-social fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/big_ecuador/"><i class="fab fab-icon-social fa-instagram"></i></a>
+            <!--<a href="#"><i class="fab fab-icon-social fa-twitter"></i></a>
             <a href="#"><i class="fab fab-icon-social fa-tiktok"></i></a>
-            <a href="#"><i class="fab fab-icon-social fa-youtube"></i></a>
+            <a href="#"><i class="fab fab-icon-social fa-youtube"></i></a>-->
         </div>
         @if (request()->routeIs('user.home'))
         <div class="video-example d-none d-sm-block">
