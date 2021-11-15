f5c53fed4f82b6ba3ce015fac65b3592d68abe33
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-15 17:49:22 -0500

    Mejora de diseño de email de confirmación

--- resources/views/emails/users/confirmation_code.blade.php
+++ resources/views/emails/users/confirmation_code.blade.php
@@ -22,19 +22,23 @@
     </head>
 
     <body>
-        <div id="app" class="">
-            <img style="width: 25%"  src="{{ asset('img/Slogan.png') }}" alt="">
-            <div class="rounded p-5 m-5" style="width: 400px; height: 450px; background: white;">
+        <div id="app" class="d-flex flex-column align-items-center justify-content-center">
+            <div class="text-center">
+                <img style="width: 70%"  src="{{ asset('img/Slogan.png') }}" alt="">
+            </div>
+            
+            <div class="rounded p-5 m-5" style="width: 450px; height: 550px; background: white;">
                 <h4><strong>Hola {{ $name }} {{ $lastname }},</strong></h4>    
-                <p style="font-size: 16px;">Gracias por registrarte en <strong>BIG Promo!</strong><p>
-                <p style="font-size: 16px;">Por favor confirma tu correo electrónico.</p>
-                <p style="font-size: 16px;">Para ello simplemente debes hacer click en el siguiente enlace:</p>
-
-                <a href="{{ url('/register/verify/' . $confirmation_code) }}">
+                <p style="font-size: 18px;">Gracias por registrarte en <strong>BIG Promo!</strong><p>
+                <p style="font-size: 18px;">Por favor confirma tu correo electrónico.</p>
+                <p style="font-size: 18px;">Para ello simplemente debes hacer click en el siguiente enlace:</p>
+                
+                <div class="d-flex flex-column align-items-center justify-content-center">
+                <a class="btn btn-lg btn-primary p-3" href="{{ url('/register/verify/' . $confirmation_code) }}">
                     Click para confirmar tu email
                 </a>
                 <br><br><img class="text-center" style="width: 25%" src="{{ asset('img/logo.png') }}" alt="">   
-                 
+                </div>
             </div>
             
         </div>
@@ -45,6 +49,10 @@
         overflow-x: hidden;
         background: rgb(253,13,27);
         background: radial-gradient(circle, rgba(253,13,27,1) 0%, rgba(172,3,13,1) 100%);
+    }
+    .btn-primary:hover {
+        border-color: rgb(253,13,27);
+        background-color: rgb(253,13,27) !important;
     }
 </style>
 </html>
