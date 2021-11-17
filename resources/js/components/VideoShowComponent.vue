523c90bbe4d5536045a76eed443d5a9a03f7ff99
Cesar Montenegro <montenegroback@gmail.com>
2021-11-16 20:33:04 -0400

    fix: video promotions

--- resources/js/components/VideoShowComponent.vue
+++ resources/js/components/VideoShowComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="modal-video" class="modal fade" data-backdrop="static" data-keyboard="false">
         <a href="#" @click="closeModal()" class="close-modal-btn p-3 float-right text-white"><i class="far fa-3x fa-times-circle"></i></a>
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content bg-dark">
-                <video width="100%" height="400" controls>
+                <video width="100%" height="450" controls>
                     <source :src="videoSrc" type="video/mp4">
                 </video>
             </div>
