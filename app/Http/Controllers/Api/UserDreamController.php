88447ec586aabb4e1f3f6be2a2249a0d913c050d
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-16 22:45:21 -0500

    Mejora en la subida del video

--- app/Http/Controllers/Api/UserDreamController.php
+++ app/Http/Controllers/Api/UserDreamController.php
@@ -10,22 +10,40 @@ class UserDreamController extends Controller
 {
     public function store(Request $request)
     {
-        $data = new UserDream;
-        $data->user_id = auth()->user()->id;
-
         if ($request->hasFile('file'))
         {
+            $data = new UserDream;
+            $data->user_id = auth()->user()->id;
+
             $file = $request->file->store('public');
-
+            $extension = File::extension($file->getClientOriginalName(), $file->getMimetype());
             $dream = $request->file('file')->store('videos', ['disk' => 'videos']);
-            $data->dream = "/".$dream;
+            $data->dream = "/".$dream.$extension;
+
+
+            // Get filename with the extension
+            $filenameWithExt = $request->file('file')->getClientOriginalName();
+            //Get just filename
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            // Get just ext
+            $extension = $request->file('file')->getClientOriginalExtension();
+            // Filename to store
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+            // Upload Image
+            $path = $request->file('file')->storeAs('public/videos',$fileNameToStore);
+
+            $data->dream = "/".$fileNameToStore;
+            
+            $data->save();
+            return response()->json([
+                'success' => true,
+                'message' => 'Sueño registrado exitosamente',
+                'data'    => [],
+            ], 200);
         }
-
-        $data->save();
-
         return response()->json([
-            'success' => true,
-            'message' => 'Sueño registrado exitosamente',
+            'success' => false,
+            'message' => 'Ocurrió un error registrando el sueño',
             'data'    => [],
         ], 200);
     }
