e093b3df0cedeb611c734ad7db33c87b6938ff0c
Cesar Montenegro <montenegroback@gmail.com>
2021-11-18 21:55:26 -0400

    fiX: transition component main component

---
+++ resources/js/components/WrapperTransitionComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition name="fade" appear>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.5s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
