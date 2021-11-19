e1fd384956396f36bc23fd139c1fb8946796269e
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-19 14:48:00 -0500

    Correccion recorder controller 5

--- app/Http/Controllers/Api/UserDreamController.php
+++ app/Http/Controllers/Api/UserDreamController.php
@@ -38,10 +38,10 @@ class UserDreamController extends Controller
         $data->user_id = auth()->user()->id;
 
         $file = $request->file('video')->store('videos', ['disk' => 'videos']);
-        $filename = pathinfo($file->hashName(), PATHINFO_FILENAME);
+        $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         
 
-        FFMpeg::fromFilesystem(Storage::disk('videos'))->open('videos/'.$file->hashName())->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264('libmp3lame', 'libx264'))->save('converted/'.$filename.'.mp4');
+        FFMpeg::fromFilesystem(Storage::disk('videos'))->open('videos/'.$file->getClientOriginalName())->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264('libmp3lame', 'libx264'))->save('converted/'.$filename.'.mp4');
         $data->dream = '/videos/converted/'.$filename.'.mp4';
 
         $data->save();
