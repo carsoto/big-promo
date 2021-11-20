1363ede1937c19498ed9915a5ed532607b38a74f
Carmen Soto <dev.carmensoto@gmail.com>
2021-11-20 12:52:04 -0500

    Video recorder 20 seg - sin exportar a mp4

--- app/Http/Controllers/Api/UserDreamController.php
+++ app/Http/Controllers/Api/UserDreamController.php
@@ -41,8 +41,9 @@ class UserDreamController extends Controller
         $filename = pathinfo($file->hashName(), PATHINFO_FILENAME);
         
 
-        FFMpeg::fromFilesystem(Storage::disk('videos'))->open('videos/'.$file->hashName())->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264('libmp3lame', 'libx264'))->save('converted/'.$filename.'.mp4');
-        $data->dream = '/converted/'.$filename.'.mp4';
+        //FFMpeg::fromFilesystem(Storage::disk('videos'))->open('videos/'.$file->hashName())->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264('libmp3lame', 'libx264'))->save('converted/'.$filename.'.mp4');
+        //$data->dream = '/converted/'.$filename.'.mp4';
+        $data->dream = '/videos/'.$filename;
 
         $data->save();
 
